7731b699b903aabe96331806c2ad875a97323fc5
Ray <love4026@gmail.com>
2020-01-18 16:05:28 +0800

    Access-Control-Allow-Headers:*

--- updata.php
+++ updata.php
@@ -7,7 +7,8 @@ require_once 'defines.php';
 // ini_set('display_startup_errors', TRUE);
 header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Methods:OPTIONS, GET, POST'); // 允许option，get，post请求
-header('Access-Control-Allow-Headers:x-requested-with'); // 允许x-requested-with请求头
+header('Access-Control-Allow-Headers:*');
+// header('Access-Control-Allow-Headers:x-requested-with, content-type'); // 允许x-requested-with请求头
 
 $url_referer = isset($_REQUEST['referer'])?$_REQUEST['referer']:'';
 $http_referer = isset($_SERVER['HTTP_REFERER'])?$_SERVER['HTTP_REFERER']:'';
